b722e562a8369d902f1996a95bdf97b7e9d5b014
Anastasiia <kvasovets.pn@ucu.edu.ua>
2025-12-01 17:11:29 +0200

    Читання графа + базова структура
    Реалізувала структуру даних для графа (список суміжності / список ребер).
    Реалізувала функцію:
    read_graph_from_csv(filename, oriented=False)
    Підтримка орієнтованих і неорієнтованих графів.
    Валідація введення (немає повторів, коректні вершини).
    Структура графа — це фундамент. Я продумала формат, перевірки, підтримку двох типів графів.

---
+++ read_graph_from_csv.py
@@ -0,0 +1,52 @@
+def read_graph_from_csv(filename:str, oriented:bool=False)\
+      -> tuple[dict[str, set[str]], set[tuple[str, str]]]:
+    """
+    Reads a graph from a CSV file (NodeA,NodeB per line)\
+          into an adjacency list and an edge set.
+
+    Args:
+        filename (str): Path to the CSV file. Each line\
+              must be 'NodeA,NodeB'.
+        oriented (bool, optional): If True, the graph is\
+              directed. Defaults to False (undirected).
+
+    Returns:
+        tuple[dict[str, set[str]], set[tuple[str, str]]]:
+            (adjacency_dict, edge_set)
+            - Adjacency dict (connections): Keys are nodes,\
+                  values are sets of neighbors.
+            - Edge set (edges): All (source, destination)\
+                  tuples.
+    """
+
+    connections = {}
+
+    edges = set()
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        file = file.readlines()
+
+    for line in file:
+
+        # Nodes are written by comma
+        line = line.strip().split(',')
+        length = len(line)
+
+        # There are no more than 2 nodes in each line
+        if length > 2 or length < 2:
+            continue
+        note1, note2 = line[0], line[-1]
+        if note1 in connections:
+            connections[note1] |= {note2}
+        else:
+            connections |= {note1: {note2}}
+        edges |= {(note1, note2)}
+        if not oriented:
+            if note2 in connections:
+                connections[note2] |= {note1}
+            else:
+                connections |= {note2: {note1}}
+            edges |= {(note2, note1)}
+
+    return connections, edges
+print(read_graph_from_csv('graph.csv', True))
